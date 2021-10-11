a24122a3749c1b1bc1ec47d34fad3375284e7fd3
Tony Zhang <tozhang665@gmail.com>
2021-10-11 02:44:08 -0700

    added tweets

--- frontend/src/reducers/tweets_reducer.js
+++ frontend/src/reducers/tweets_reducer.js
@@ -5,22 +5,24 @@ const RECEIVE_TWEETS = "RECEIVE_TWEETS";
 const RECEIVE_USER_TWEETS = "RECEIVE_USER_TWEETS";
 const RECEIVE_NEW_TWEET = "RECEIVE_NEW_TWEET";
 
-const TweetsReducer = (state = { all: {}, user: {}, new: undefined }, action) => {
+// src/reducers/tweets_reducer.js
+
+  const TweetsReducer = (state = { all: {}, user: {}, new: undefined }, action) => {
     Object.freeze(state);
     let newState = Object.assign({}, state);
-    switch (action.type) {
-        case RECEIVE_TWEETS:
-            newState.all = action.tweets.data;
-            return newState;
-        case RECEIVE_USER_TWEETS:
-            newState.user = action.tweets.data;
-            return newState;
-        case RECEIVE_NEW_TWEET:
-            newState.new = action.tweet.data
-            return newState;
-        default:
-            return state;
+    switch(action.type) {
+      case RECEIVE_TWEETS:
+        newState.all = action.tweets.data;
+        return newState;
+      case RECEIVE_USER_TWEETS:
+        newState.user = action.tweets.data;
+        return newState;
+      case RECEIVE_NEW_TWEET:
+        newState.new = action.tweet.data
+        return newState;
+      default:
+        return state;
     }
-};
-
-export default TweetsReducer;
+  };
+  
+  export default TweetsReducer;
--- frontend/src/util/tweet_api_util.js
+++ frontend/src/util/tweet_api_util.js
@@ -1,13 +1,15 @@
-   import axios from 'axios';
+// src/util/tweet_api_util.js
+
+import axios from 'axios';
 
 export const getTweets = () => {
-    return axios.get('/api/tweets')
+  return axios.get('/api/tweets')
 };
 
 export const getUserTweets = id => {
-    return axios.get(`/api/tweets/user/${id}`)
+  return axios.get(`/api/tweets/user/${id}`)
 };
 
 export const writeTweet = data => {
-    return axios.post('/api/tweets/', data)
+  return axios.post('/api/tweets/', data)
 }
