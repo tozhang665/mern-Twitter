2aa5222e65ad5307406ae35a02da2e83e610af2a
Tony Zhang <tozhang665@gmail.com>
2021-10-10 23:23:35 -0700

    added validations and passport

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const users = require("./config/routes/api/users")
 const tweets = require("./config/routes/api/tweets")
 const db = require('./config/keys').mongoURI
 const bodyParser = require('body-parser');
+const passport = require('passport');
 
 const app = express();
 
@@ -12,16 +13,22 @@ mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server is running on port ${port}`));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
-
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  
+  app.use(passport.initialize());
+  require('./config/passport')(passport);
+  
 
 app.use("/api/users", users);
 app.use("/api/tweets", tweets);
 
-app.get("/", (req,res) => res.send("Hello World!"));
+
+const port = process.env.PORT || 5000;
+
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
+
 
 
--- config/routes/api/tweets.js
+++ config/routes/api/tweets.js
@@ -1,8 +1,30 @@
 const express = require("express");
 const router = express.Router();
+const passport = require("passport")
+const validateTweetInput = require("../../../validation/tweet")
+const Tweet = require("../../../models/Tweet")
 
 
 router.get("/test", (req, res) => res.json({ msg: "This is the tweets route" }));
+
+  
+router.post("/",
+  passport.authenticate("jwt",{session:false}),
+  (req,res)=>{
+    console.log("hit1")
+    const {isValid, errors} = validateTweetInput(req.body);
+    console.log("hit2")
+    if(!isValid){
+      return res.status(400).json(errors);
+    }
+    console.log("hit3")
+    const newTweet = new Tweet({
+      user: req.user.id,
+      text: req.body.text
+    })
+
+  }
+)
 
 
 module.exports = router;
