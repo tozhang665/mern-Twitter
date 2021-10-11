04818832d4fee26069a33b9ee9f21540d6a6811f
Tony Zhang <tozhang665@gmail.com>
2021-10-11 00:36:55 -0700

    added app.js

---
+++ frontend/src/components/app.js
@@ -0,0 +1,15 @@
+// src/components/app.js
+
+import React from 'react';
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
+import { Switch } from 'react-router-dom';
+
+import MainPage from './main/main_page';
+
+const App = () => (
+    <Switch>
+        <AuthRoute exact path="/" component={MainPage} />
+    </Switch>
+);
+
+export default App;
